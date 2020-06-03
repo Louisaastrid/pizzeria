a15373595cb20ca2159b3172ff6aaad8bb4a44ce
Louisaastrid <60742071+Louisaastrid@users.noreply.github.com>
2020-06-03 17:35:13 +0200

    feat: 🎸 composant header

---
+++ src/Header/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Badge,
+} from "@material-ui/core";
+//import MailIcon from "@material-ui/icons/Mail";
+import { makeStyles } from "@material-ui/core/styles";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { number } from "prop-types";
+
+const useStyles = makeStyles({
+  title: {
+    flexGrow: 1,
+  },
+});
+export default function Header({ shoppingCartCount }) {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          Pizzeria Astrid
+        </Typography>
+        <IconButton aria-label={`${shoppingCartCount} pizza`} color="inherit">
+          <Badge badgeContent={shoppingCartCount} color="secondary">
+            <ShoppingCartIcon />
+          </Badge>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
+Header.defaultProps = {
+  shoppingCartCount: 0,
+};
+Header.propTypes = {
+  shoppingCartCount: number,
+};
